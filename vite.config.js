ed67d02d02d78f048d2f6a4584245ad122a2be08
swagath0 <sswagath46@gmail.com>
2025-10-13 22:26:44 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/netflix-clone-django-react/' : '/',
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Use relative paths for production
 });
