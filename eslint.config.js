d12b27c0d73a3dab3fb3e021c47679209d0fae14
swagath0 <sswagath46@gmail.com>
2025-10-14 20:08:47 +0530

    Updated all JSX files inside and outside components with BASE_URL and fixed Playvideo

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    parser: '@babel/eslint-parser', // use Babel parser for JSX
+    parserOptions: {
+      requireConfigFile: false,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+    },
+    plugins: ['react', 'react-hooks'],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      react.configs.recommended,
+      reactHooks.configs.recommended,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/react-in-jsx-scope': 'off', // React 17+ doesn't need React import
     },
   },
-])
+]);
